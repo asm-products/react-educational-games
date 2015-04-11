7f93a9506b75a72bba34d26e165fb5110eafe75c
Sami Greenbury <dude@patabugen.co.uk>
2015-04-11 13:43:40 +0100

    Started Angularigying

--- src/js/components/readme.md
+++
@@ -1,2 +0,0 @@
-Create one file for each Component, they will be concatinated by the build
-script into one big file.
--- src/js/layout.js
+++ src/js/layout.js
@@ -1,18 +1,21 @@
 /**
 * Code relating to the visual Layout
 **/
-(function($){
-	function resizeLayout()
+var Layout = {
+	resizeLayout: function()
 	{
-		var height = $(window).height();
-		height = height - $('#header').height();
-		height = height - $('#footer').height();
-		height = height * 0.95;
+		var height = Zepto(window).height();
+		height = height - Zepto('#header').height();
+		height = height - Zepto('#footer').height();
+		height = height * 0.90;
 		$('#middle > .column').css('height', height + 'px');
 	}
+};
+
+(function($){
 	$(function(){
 		$(window).on('resize', function(){
-			resizeLayout();
-		}).trigger('resize');
+			Layout.resizeLayout();
+		});
 	});
 }(Zepto));
